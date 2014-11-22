5394f34c7a744e5d9a02d56cf9b40da4126c5093
Shafqat Ahmed <shafqatahmed@gmail.com>
2014-11-22 08:01:40 +0600

    Update fix

--- trunk/web/app.js
+++ trunk/web/app.js
@@ -1,20 +1,17 @@
 /* Get config and load config */
 
-var activeConfig = require ("./config.active.js");
-
+var activeConfig = require ("./modules/config");
 var express = require('express');
 
+/* Configure Express */
 /* Preparing swig */
 swig = require('swig');
 
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var loggerMorgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var api = require('./routes/api');
 
 var app = express();
 
@@ -30,80 +27,39 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view cache', false);
 swig.setDefaults({ cache: false });
 
-
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+app.use(loggerMorgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 //app.use(require('less-middleware')(path.join(__dirname, 'public')));
 
 var db = require("./data/dbaccess.js");
 app.use(function(req, res, next) {
     req.db = db;
-    console.log("adding db");
     next();
 });
 
+//var x = require('./modules/context');
+//var y = x(app, activeConfig);
+//console.log(["x",x,y]);
+
+var context = new require('./modules/context')(app, activeConfig);
+var logger = new require("./modules/logging") (context);
 
 app.use("/public", express.static(path.join(__dirname, 'public')));
+
+var routes = require('./routes/index');
+var api = require('./routes/api');
+var user = require('./routes/user');
+
 app.use('/', routes);
 app.use("/api",api);
+app.use("/user", user);
 
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// TODO: Move into a different router
-
-app.post('/login',
-    passport.authenticate('local', {
-        successRedirect: '/loginSuccess',
-        failureRedirect: '/loginFailure'
-    })
-);
-
-
-app.get('/loginFailure', function (req, res, next) {
-    res.send('Failed to authenticate');
-});
-
-app.get('/loginSuccess', function (req, res, next) {
-    console.log(req);
-    res.send('Successfully authenticated - ' + JSON.stringify(req.passport));
-});
-
-passport.serializeUser(function (user, done) {
-    console.log("serializeUser")
-    done(null, user);
-});
-
-passport.deserializeUser(function (user, done) {
-    console.log("deserializeUser")
-    done(null, user);
-});
-
-passport.use(new LocalStrategy(
-    function (username, password, done) {
-        return done(null, {username: username});
-        /*
-        User.findOne({ username: username }, function (err, user) {
-            if (err) {
-                return done(err);
-            }
-            if (!user) {
-                return done(null, false, { message: 'Incorrect username.' });
-            }
-            if (!user.validPassword(password)) {
-                return done(null, false, { message: 'Incorrect password.' });
-            }
-            return done(null, {username: username});
-        });
-        */
-    }
-));
+// Authentication stack
+Authentication = require("./modules/authentication")
+var auth = new Authentication (app, "/user");
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
